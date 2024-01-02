cb27e296c9080a3c693105eaf858da937e08d09f
Jaden Botha <jadendeanbotha@gmail.com>
2024-01-02 10:40:27 +0200

    Created my first View elements

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4d67938578b1301c71787abd044661ccf411d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf21ad4261793d01b203c912d83ce50290c06d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
